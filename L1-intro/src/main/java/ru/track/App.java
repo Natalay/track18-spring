81d4fbe5ac7062d8a8f40e577765a86978381da2
Natalay <sorokinan86@outlook.com>
2018-02-28 02:44:12 +0300

    have done 1 hometask:)

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -1,6 +1,6 @@
 package ru.track;
 
-import com.mashape.unirest.http.*
+import com.mashape.unirest.http.*;
 
 
 /**
@@ -15,25 +15,30 @@ import com.mashape.unirest.http.*
 public class App {
 
     public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_GITHUB = "github";
-    public static final String FIELD_EMAIL = "email";
+    public static final String FIELD_NAME = "Natalay";
+    public static final String FIELD_GITHUB = "Natalay";
+    public static final String FIELD_EMAIL = "sorokinan86@outlook.com";
 
     public static void main(String[] args) throws Exception {
        // 1) Use Unirest.post()
-        Unirest.post(URL);
-        Unirest
-             //   .queryString("name", "Mark")
-               // .field("last", "Polo")
-               // .asJson()
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                //  .header("accept", "application/json")
+                .queryString("name", FIELD_NAME)
+                .field("github", FIELD_GITHUB)
+                .field("email", FIELD_EMAIL)
+                .asJson();
+
         // 2) Get response .asJson()
+        System.out.println(jsonResponse.getHeaders());
+
         // 3) Get json body and JsonObject
+        System.out.println(jsonResponse.getBody());
+
         // 4) Get field "success" from JsonObject
 
 
-
-
         boolean success = false;
+        Unirest.shutdown();
     }
 
 }
