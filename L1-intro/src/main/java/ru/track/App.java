eb074d887f3603fde7e93fbd923d7f9f61641c6d
Natalay <sorokinan86@outlook.com>
2018-02-28 03:04:19 +0300

    changed App.java in final system.out

--- L1-intro/src/main/java/ru/track/App.java
+++ L1-intro/src/main/java/ru/track/App.java
@@ -29,13 +29,11 @@ public class App {
                 .asJson();
 
         // 2) Get response .asJson()
-        System.out.println(jsonResponse.getHeaders());
-
-        // 3) Get json body and JsonObject
         System.out.println(jsonResponse.getBody());
+        // 3) Get json body and JsonObject
+        System.out.println(jsonResponse.getBody().getObject().get("success"));
 
         // 4) Get field "success" from JsonObject
-
 
         boolean success = false;
         Unirest.shutdown();
